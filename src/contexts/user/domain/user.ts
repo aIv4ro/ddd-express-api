dd8079dc94e3dd06132a94978540cbbc4576934c
Alvaro <alvaromagu2002@gmail.com>
2023-05-18 20:36:39 +0200

    Extends AggregateRoot in User

--- src/contexts/user/domain/user.ts
+++ src/contexts/user/domain/user.ts
@@ -1,11 +1,18 @@
-export class User {
+import { AggregateRoot } from '../../shared/domain/aggregate-root'
+
+export class User extends AggregateRoot {
   readonly id: string
   readonly username: string
   readonly password: string
 
   constructor (id: string, username: string, password: string) {
+    super()
     this.id = id
     this.username = username
     this.password = password
+  }
+
+  toPrimitives (): Record<string, any> {
+    return { id: this.id, username: this.username, password: this.password }
   }
 }
