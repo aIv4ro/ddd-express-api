a476c248dbda32850784888d0bb07c4e2c402295
Alvaro <alvaromagu2002@gmail.com>
2023-06-10 20:02:24 +0200

    Add base mongo repository

--- src/contexts/shared/infrastructure/persistance/mongodb/mongo-repository.ts
+++ src/contexts/shared/infrastructure/persistance/mongodb/mongo-repository.ts
@@ -1,7 +1,24 @@
-import { type MongoClient } from 'mongodb'
+import { ObjectId, type Collection, type MongoClient } from 'mongodb'
+import { type AggregateRoot } from '../../../domain/aggregate-root'
 
-export abstract class MongoRepository {
+export abstract class MongoRepository<T extends AggregateRoot> {
   constructor (
     protected readonly mongoClient: MongoClient
   ) {}
+
+  protected abstract collectionName (): string
+  protected get collection (): Collection { return this.mongoClient.db().collection(this.collectionName()) }
+
+  protected async findById (id: string): Promise<T | null> {
+    return await this.collection.findOne<T>({ _id: new ObjectId() })
+  }
+
+  protected async create (aggregate: Omit<T, 'id'>): Promise<string> {
+    const { insertedId } = await this.collection.insertOne(aggregate)
+    return insertedId.toString()
+  }
+
+  protected async update (id: any, aggregate: T): Promise<void> {
+    await this.collection.updateOne({ _id: id }, { $set: aggregate })
+  }
 }
